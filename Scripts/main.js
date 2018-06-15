364b56445aaf8f559b0b81fd083b8ae434e59a85
Gaurav Aggarwal <aggarwal.gaurav10@gmail.com>
2018-06-15 21:52:56 +0530

    Index Page Completed

--- Scripts/main.js
+++ Scripts/main.js
@@ -46,3 +46,40 @@ function rightCheck(){
 
 startSlide();
 setInterval(rightCheck,time);
+
+
+// Form
+document.getElementById('btn-form').addEventListener('click',formReset);
+
+function formReset(e){
+    e.preventDefault();
+    document.getElementById('inputName1').value='';
+    document.getElementById('inputName2').value='';
+    document.getElementById('inputEmail').value='';
+    document.getElementById('inputContact').value='';
+    document.getElementById('inlineRadio1').checked=false;
+    document.getElementById('inlineRadio2').checked=false;
+}
+
+
+// BMI
+document.getElementById('output').style.visibility='hidden';
+document.getElementById('bmi-btn').addEventListener('click',bmiCalc);
+document.getElementById('bmi-resetbtn').addEventListener('click',bmiReset);
+
+function bmiCalc(e){  
+    e.preventDefault();
+    let height=document.getElementById('height').value/100;
+    let weight=document.getElementById('weight').value;
+    document.getElementById('output').style.visibility='visible';
+    document.getElementById('bmiOutput').innerHTML= weight/(height*height);
+
+}
+
+function bmiReset(e){
+    e.preventDefault();
+    document.getElementById('output').style.visibility='hidden';
+    document.getElementById('height').value='';
+    document.getElementById('weight').value='';
+
+}
